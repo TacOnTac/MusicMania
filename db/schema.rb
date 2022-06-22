682f4c4363fc7e5e2d5ceeec2efda4ef941a57fd
TaconTac <djx21ca@gmail.com>
2022-06-22 17:59:08 -0400

    added genre option to album form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_215508) do
+ActiveRecord::Schema.define(version: 2022_06_22_215754) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_215508) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "artist_id"
+    t.integer "genre_id"
   end
 
   create_table "artists", force: :cascade do |t|
