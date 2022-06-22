694f5691839741eb1dbe8ad777e470566c989a23
TaconTac <djx21ca@gmail.com>
2022-06-22 18:05:38 -0400

    added album option to song form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_215754) do
+ActiveRecord::Schema.define(version: 2022_06_22_220409) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_215754) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "genre_id"
     t.integer "artist_id"
+    t.integer "album_id"
   end
 
 end
