3826ed208d2794dc506ac878452b015769a4f3a2
TaconTac <djx21ca@gmail.com>
2022-06-22 17:57:24 -0400

    added artist option to album form

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_215014) do
+ActiveRecord::Schema.define(version: 2022_06_22_215508) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
     t.integer "year"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "artist_id"
   end
 
   create_table "artists", force: :cascade do |t|
