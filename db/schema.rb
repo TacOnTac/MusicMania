812dbb07845c7f7c05792b34cad7de814d862017
TaconTac <djx21ca@gmail.com>
2022-06-22 11:00:36 -0400

    added genre option to song form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_005535) do
+ActiveRecord::Schema.define(version: 2022_06_22_145205) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_005535) do
     t.integer "BPM"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre_id"
   end
 
 end
