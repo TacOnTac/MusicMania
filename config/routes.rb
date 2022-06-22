7cae7ced9ab9c6a22124819b5f98a08a846c440c
TaconTac <djx21ca@gmail.com>
2022-06-22 14:20:42 -0400

    change homepage

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :genres
   resources :songs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Change Default Home Page to songs
+  root to: "songs#index"
+
 end
